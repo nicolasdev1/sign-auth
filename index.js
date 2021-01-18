b62de51abf20305d28a4e1c3408e6165243b9600
Nicolas Carvalho <nicolaspessoal@icloud.com>
2021-01-18 12:33:40 -0300

    Add DB_PORT

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 })
 
 app.post('/register', (request, response) => {
